2f096b81a11a6816d7e70306b6811f15a586546f
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-03-24 03:29:03 +0800

    techtalk

---
+++ dotNet.跨平台开发移植初探.2018/Cross-Platforms.Samples/TestConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microshaoft;
+
+namespace TestConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
